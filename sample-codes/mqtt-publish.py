5df1e71202590bf31bcc7eada98a64e2caf26cb3
Samarth <me@samteck.net>
2020-09-23 23:29:30 +0530

    publish working

--- sample-codes/mqtt-publish.py
+++ sample-codes/mqtt-publish.py
@@ -3,9 +3,8 @@
 import paho.mqtt.client as paho
 
 # Eclipse Broker Address
-broker="***********"
+broker="***"
 port=1883
-#username_pw_set(username="*******",password="******")
 
 def on_publish(client,userdata,result):             #create function for callback
     print("data published \n")
@@ -13,6 +12,7 @@ def on_publish(client,userdata,result):             #create function for callbac
 
 client1= paho.Client("RPi-1")                           #create client object
 client1.on_publish = on_publish                          #assign function to callback
+client1.username_pw_set(username="***",password="***")
 client1.connect(broker,port)                                 #establish connection
 
-ret= client1.publish("John Snow","on")                   #publish
+ret= client1.publish("john-snow","on")                   #publish
