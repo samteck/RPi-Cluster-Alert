a0ac1b3c83b881458401f72063524a5427b2ef2c
Samarth <me@samteck.net>
2020-09-25 02:01:06 +0530

    creating dist packages

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RPi-cluster-tracking',
+    version='1.0',
+    author='Samarth Gupta',
+    author_email='me@samteck.net',
+    packages=find_packages(),
+)
