01ccb1fd73b5e86ab4da6dcea5eed7ee936a8504
Samarth <me@samteck.net>
2020-09-24 23:27:51 +0530

    modified the tracker file

--- tracker.py
+++ tracker.py
@@ -9,6 +9,14 @@ import configparser
 import argparse
 import paho.mqtt.client as paho
 import time
+import json
+import RPi.GPIO as IO
+
+# setting mode for GPIO pins and disabling intial GPIO warnings
+led = 2
+IO.setwarnings(False)
+IO.setmode(IO.BCM)
+IO.setup(led,IO.OUT)
 
 def on_connect(client, userdata, flags, rc):
     """Callback for when a device connects."""
@@ -17,7 +25,27 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
     """Paho callback when a message is received."""
     print(msg.topic+" "+str(msg.payload))
-    
+    json_data = json.loads(msg.payload)
+    check_breakdown(json_data['status'])
+
+#check if other pi is down or not
+def check_breakdown(str):
+    if (str == "on"):
+        print('its ON baby')
+        IO.output(led,IO.LOW)
+    elif (str == "off"):
+        print('its GONE baby')
+        i = 0
+        while i<10:
+            glitter()
+            i+=1
+
+# led light on-off            
+def glitter():
+    IO.output(led,IO.HIGH)
+    time.sleep(.5)
+    IO.output(led,IO.LOW)
+    time.sleep(.5)
 
 def main():
     try:
@@ -44,11 +72,13 @@ def main():
         port = int(config.get('MQTT', 'PORT'))
         username = config.get('MQTT', 'USERNAME')
         password = config.get('MQTT', 'PASSWORD')
+        #led = int(config.get('RPi', 'LED'))
+        #print(led)
     except:
         print('please check the config file')
 
     try:
-        client2= paho.Client(client)                           #create client object                          
+        client2= paho.Client(client,clean_session=False)                           #create client object                          
         client2.on_connect = on_connect                       #assign function to callback
         client2.on_message = on_message                       #assign function to callback
         client2.username_pw_set(username=username,password=password)
