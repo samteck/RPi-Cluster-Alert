414f65abe6bbfaf598a10e0d18a4d314fbbd43c9
Samarth <me@samteck.net>
2020-09-24 17:10:54 +0530

    agent publish working successfully

--- agent.py
+++ agent.py
@@ -1,3 +1,10 @@
+######################
+#Project Title : RPi Cluster Alert
+#Module        : agent to send heartbeat signals
+#Author        : Samarth Gupta
+#Date          : 24/09/2020
+#Version       : 1.0
+
 import configparser
 import argparse
 import paho.mqtt.client as paho
@@ -5,51 +12,56 @@ import time
 
 def on_connect(unused_client, unused_userdata, unused_flags, rc):
     """Callback for when a device connects."""
-    print('on_connect')
-
+    print('sucessfully connected')
+ 
 def on_publish(unused_client, unused_userdata, unused_mid):
     """Paho callback when a message is sent to the broker."""
-    print('on_publish')
-
+    print('status message published')
 
 def main():
-    # Construct the argument parser
-    ap = argparse.ArgumentParser()
+    try:
+        # Construct the argument parser
+        ap = argparse.ArgumentParser()
 
-    # Add the arguments to the parser
-    ap.add_argument("-c", "--client", required=True, help="mqtt client name")
-    ap.add_argument("-t", "--topic", required=True, help="raspberry pi topic")
-    #ap.add_argument("-tt", "--track-rpi", required=True, help="track raspberry pi topic")
-    args = vars(ap.parse_args())
+        # Add the arguments to the parser
+        ap.add_argument("-c", "--client", required=True, help="mqtt client name")
+        ap.add_argument("-t", "--topic", required=True, help="raspberry pi topic")
+        #ap.add_argument("-tt", "--track-rpi", required=True, help="track raspberry pi topic")
+        args = vars(ap.parse_args())
 
-    client = str(args['client'])
-    topic = str(args['topic'])
+        client = str(args['client'])
+        topic = str(args['topic'])
+    except:
+        print('please check the arguments passed')
 
-    print(client)
-    print(topic)
+    try:
+        # Read local `config.ini` file.
+        config = configparser.ConfigParser()                                     
+        config.read('./config.ini')
 
-    # Read local `config.ini` file.
-    config = configparser.ConfigParser()                                     
-    config.read('./config.ini')
+        # Get values from our .ini file
+        broker = config.get('MQTT', 'HOST')
+        port = int(config.get('MQTT', 'PORT'))
+        username = config.get('MQTT', 'USERNAME')
+        password = config.get('MQTT', 'PASSWORD')
+    except:
+        print('please check the config file')
 
-    # Get values from our .ini file
-    broker = config.get('MQTT', 'HOST')
-    port = config.get('MQTT', 'PORT')
-    username = config.get('MQTT', 'USERNAME')
-    password = config.get('MQTT', 'PASSWORD')
-
-    print(broker + port)
-
-    client1= paho.Client(client)                           #create client object                          
-    client1.on_connect = on_connect                       #assign function to callback
-    client1.on_publish = on_publish                       #assign function to callback
-    client1.username_pw_set(username=username,password=password)
-    client1.connect(broker,port)                                 #establish connection
+    try:
+        client1= paho.Client(client)                           #create client object                          
+        client1.on_connect = on_connect                       #assign function to callback
+        client1.on_publish = on_publish                       #assign function to callback
+        client1.username_pw_set(username=username,password=password)
+        client1.connect(broker,port)                                 #establish connection
+    except:
+        print('connection to broker unsuccessfull')
 
     while 1 :
-
-        ret= client1.publish(topic,"{\"status\":\"on\"}")
-        time.sleep(5)
+        try:
+            ret= client1.publish(topic,"{\"status\":\"on\"}")
+            time.sleep(5)
+        except:
+            print('can not publish the data')
 
 if __name__ == '__main__':
     main()
