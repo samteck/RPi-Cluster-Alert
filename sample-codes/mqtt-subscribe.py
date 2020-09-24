beb2c42503cc8438485135a18c33a028e7518669
Samarth Gupta <samarth.guptas.1995@gmail.com>
2020-09-24 11:42:54 +0530

    added subscribe code

---
+++ sample-codes/mqtt-subscribe.py
@@ -0,0 +1,19 @@
+import paho.mqtt.client as mqtt
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print(msg.topic+" "+str(msg.payload))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+client.username_pw_set(username="***",password="***")
+client.connect("***", 1883)
+
+client.subscribe("khal-drogo")
+
+client.loop_forever()
